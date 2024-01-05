cf1bdd5bf965c6025d6cc60da6d1c5e26968a7b7
Akshay51337 <khobragadeakshay03@gmail.com>
2024-01-05 08:35:07 +0530

    127 create post save feature Entity class added 4th jan 2024

---
+++ src/main/java/com/myblog/payload/PostDto.java
@@ -0,0 +1,14 @@
+package com.myblog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDto {
+    private String title;
+    private String description;
+    private String content;
+}
